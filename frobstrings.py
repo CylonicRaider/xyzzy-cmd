49586a78d412591ec0828a005e21b039811d4598
CylonicRaider <CylonicRaider@users.noreply.github.com>
2016-11-10 15:21:06 +0100

    Commence template parser

--- frobstrings.py
+++ frobstrings.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: ascii -*-
 
-import sys, subprocess
+import sys, re, subprocess
 
 ESCAPES = {"'": "'", '"': '"', '?': '?', '\\': '\\', 'a': '\a', 'b': '\b',
            'f': '\f', 'n': '\n', 'r': '\r', 't': '\t', 'v': '\v'}
 # Octals are up to three characters long and end at the first non-octal.
 # Hexadecimals can be arbitrarily long.
 
+TMPL_TOKEN = re.compile(r'(?P<ident>[a-zA-Z_][a-zA-Z0-9_]*)|(?P<op>=)|'
+    r'(?P<str>"(?:[^"\\\\]|\\\\.)*")|(?P<comment>//.*$)')
+
 def parse_tmpl(inpt):
-    raise NotImplementedError
+    for rawline in inpt:
+        # End when no lines left
+        if not rawline: break
+        line = rawline.strip()
+        # Ignore empty lines
+        if not line: continue
+        # Respect preprocessor instructions
+        if line.startswith('#'):
+            yield ('prep', line)
+            continue
+        # Parse!
+        raise NotImplementedError
 
 def main():
     raise NotImplementedError
