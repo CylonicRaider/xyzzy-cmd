8a50d829c287bfdd51197e50e72c4a6a42afa822
CylonicRaider <CylonicRaider@users.noreply.github.com>
2016-12-07 14:57:29 +0100

    Migrate comm.c to stream FD-s

--- src/comm.c
+++ src/comm.c
@@ -16,9 +16,38 @@ void prepare_address(struct sockaddr_un *addr, socklen_t *addrlen) {
     *addrlen = sizeof(sa_family_t) + sizeof(socket_addr) - 1;
 }
 
-int send_message(int fd, const struct message *msg, int flags) {
+ssize_t read_exactly(int fd, void *buf, size_t len) {
+    char *ptr = buf;
+    ssize_t ret = 0;
+    if (len == 0) return 0;
+    while (ret != len) {
+        ssize_t rd = read(fd, ptr + ret, len - ret);
+        if (rd == -1) return -1;
+        if (rd == 0) break;
+        ret += rd;
+    }
+    return ret;
+}
+
+ssize_t write_exactly(int fd, void *buf, size_t len) {
+    char *ptr = buf;
+    ssize_t ret = 0;
+    if (len == 0) return 0;
+    while (ret != len) {
+        ssize_t wr = write(fd, ptr + ret, len - ret);
+        if (wr == -1) return -1;
+        if (wr == 0) {
+            errno = EBUSY;
+            return -1;
+        }
+        ret += wr;
+    }
+    return ret;
+}
+
+ssize_t send_message(int fd, const struct message *msg, int flags) {
     char *buf;
-    int ret = -1;
+    ssize_t ret = -1;
     if (flags & ~_COMM_MASK) {
         errno = EINVAL;
         return -1;
@@ -30,19 +59,19 @@ int send_message(int fd, const struct message *msg, int flags) {
     memcpy(buf + 8, msg->data, msg->length);
     if (! (flags & COMM_NOSCRAMBLE))
         frobl(msg->key, buf + 8, buf + 8, msg->length);
-    ret = write(fd, buf, msg->length + 8);
+    ret = write_exactly(fd, buf, msg->length + 8);
     free(buf);
     return ret;
 }
 
-int recv_message(int fd, struct message *msg, int flags) {
-    char recvbuf[65536], *recvptr;
-    int ret;
+ssize_t recv_message(int fd, struct message *msg, int flags) {
+    char recvbuf[8], *recvptr;
+    ssize_t ret;
     if (flags & ~_COMM_MASK) {
         errno = EINVAL;
         return -1;
     }
-    ret = recv(fd, recvbuf, sizeof(recvbuf), MSG_PEEK);
+    ret = read_exactly(fd, recvbuf, sizeof(recvbuf));
     if (ret == -1) return ret;
     if (ret < 8) {
         errno = EBADMSG;
@@ -52,19 +81,21 @@ int recv_message(int fd, struct message *msg, int flags) {
     recvptr = recvbuf;
     msg->key = ntohl(((union intcast *) recvptr)[0].num);
     msg->length = ntohl(((union intcast *) recvptr)[1].num);
-    if (ret != msg->length + 8) {
-        errno = EBADMSG;
-        return -1;
-    }
     if (msg->length == 0) {
         free(msg->data);
         msg->data = NULL;
     } else {
+        size_t rd;
         msg->data = realloc(msg->data, msg->length);
         if (msg->data == NULL) return -1;
+        rd = read_exactly(fd, msg->data, msg->length);
+        if (rd == -1) return -1;
+        if (rd < msg->length) {
+            errno = EBADMSG;
+            return -1;
+        }
         if (! (flags & COMM_NOSCRAMBLE))
-            defrobl(msg->key, recvbuf + 8, recvbuf + 8, msg->length);
-        memcpy(msg->data, recvbuf + 8, msg->length);
+            defrobl(msg->key, msg->data, msg->data, msg->length);
     }
     return ret;
 }
--- src/comm.h
+++ src/comm.h
@@ -26,12 +26,30 @@ union intcast {
  * An infallible convenience function. */
 void prepare_address(struct sockaddr_un *addr, socklen_t *addrlen);
 
+/* Read exactly len bytes from fd, taking as many attempts as necessary
+ * Returns the amount of bytes read (less than len on EOF), or -1 on error
+ * (having errno set).
+ * If len is zero, this returns instant success. */
+ssize_t read_exactly(int fd, void *buf, size_t len);
+
+/* Write exactly len bytes to fd
+ * Returns the amount of bytes written (i.e. len), or -1 on error (having
+ * errno set); if a single system call writes zero bytes, EBUSY is raised.
+ * If len is zero, this returns instant success. */
+ssize_t write_exactly(int fd, void *buf, size_t len);
+
 /* Send a message into the given file descriptor
- * Flags is a bitwise OR of COMM_* constants (or zero). */
-int send_message(int fd, const struct message *msg, int flags);
+ * Flags is a bitwise OR of COMM_* constants (or zero).
+ * Returns the amount of bytes read, or -1 on error (setting errno).
+ * NOTE that this can use multiple system calls. */
+ssize_t send_message(int fd, const struct message *msg, int flags);
 
 /* Receive a message from the given file descriptor
- * Flags is a bitwise OR of COMM_* constants (or zero). */
-int recv_message(int fd, struct message *msg, int flags);
+ * Flags is a bitwise OR of COMM_* constants (or zero).
+ * Returns the amount of bytes written, or -1 on error (setting errno).
+ * NOTE that this can use multiple system calls, and that their pattern
+ *      may not align with that of send_message() (i.e. don't use those
+ *      on packet sockets). */
+ssize_t recv_message(int fd, struct message *msg, int flags);
 
 #endif
