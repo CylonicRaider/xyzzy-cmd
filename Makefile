7a9f11ef892726b661bd9380602d8d11fe4422d0
CylonicRaider <CylonicRaider@users.noreply.github.com>
2016-12-09 15:27:51 +0100

    Move header files into inc

--- Makefile
+++ Makefile
@@ -5,14 +5,14 @@ LD = gcc
 STRIP = strip
 
 ifneq ($(strip $(GLIBC)),)
-CFLAGS = -Wall -Werror
+CFLAGS = -Wall -Werror -Iinc
 LDFLAGS =
 else
 # The -iwithprefix apparently magically adds the GCC include directory
 # into the path.
 CFLAGS = -O2 -g -std=c99 -flto -Wall -Werror -D__KLIBC__ -D_BITSIZE=64 \
     -iwithprefix include -nostdinc -I$(KLIBC)/include/bits64 \
-    -I$(KLIBC)/include/arch/x86_64 -I$(KLIBC)/include -Isrc \
+    -I$(KLIBC)/include/arch/x86_64 -I$(KLIBC)/include -Iinc \
     -ffunction-sections -fdata-sections -fno-asynchronous-unwind-tables
 LDFLAGS = -fwhole-program -nostdlib -static -L$(KLIBC)/lib \
     $(KLIBC)/lib/crt0.o -lc -Wl,--gc-sections
@@ -35,17 +35,18 @@ build:
 build/%.o: src/%.c | build
 	$(CC) -c -o $@ $< $(CFLAGS)
 
-src/%.frs.h src/%.frs.c: src/%.frs frobnicate script/frobstrings.py
-	script/frobstrings.py -o src/$*.frs.c -h src/$*.frs.h src/$*.frs || \
-	{ rm -f src/$*.frs.h src/$*.frs.c; false; }
+inc/%.frs.h src/%.frs.c: src/%.frs frobnicate script/frobstrings.py
+	script/frobstrings.py -o src/$*.frs.c -h inc/$*.frs.h src/$*.frs || \
+	{ rm -f inc/$*.frs.h src/$*.frs.c; false; }
 
 clean:
-	rm -rf build src/*.frs.[ch] .deps.mk frobnicate xyzzy
+	rm -rf build src/*.frs.c inc/*.frs.h .deps.mk frobnicate xyzzy
 
 # Be less noisy.
-.deps.mk: $(filter-out %.frs.c %.frs.h,$(wildcard src/*.[ch])) src/*.frs \
-    script/makedeps.py
-	@script/makedeps.py $^ frobnicate:build/frobnicate-main.o \
-	build/xyzzy-full:build/xyzzy.o > $@
+.deps.mk: $(filter-out %.frs.c %.frs.h,$(wildcard src/*.c inc/*.h)) \
+    src/*.frs script/makedeps.py
+	@INCLUDE=inc script/makedeps.py $^ \
+	frobnicate:build/frobnicate-main.o build/xyzzy-full:build/xyzzy.o \
+	> $@
 
 -include .deps.mk
--- script/makedeps.py
+++ script/makedeps.py
@@ -7,16 +7,17 @@ ALLOWED_RE = re.compile(r'^.*(?<!\.frs)\.c$')
 PROGRAM_RE = re.compile(r'^.*:.*$')
 INCLUDE_RE = re.compile(r'\s*#\s*include\s+"([^"]*)"')
 
-HEADER_RE = re.compile(r'^src/(.*)\.h$')
+HEADER_RE = re.compile(r'^inc/(.*)\.h$')
 SOURCE_RE = re.compile(r'^src/(.*)\.c$')
 OBJECT_RE = re.compile(r'^build/(.*)\.o$')
 
-HEADER_SUB = r'src/\1.h'
+HEADER_SUB = r'inc/\1.h'
 SOURCE_SUB = r'src/\1.c'
 OBJECT_SUB = r'build/\1.o'
 
-def read_deps(f):
-    basedir = os.path.dirname(f)
+def read_deps(f, basedir=None):
+    if basedir is None:
+        basedir = os.path.dirname(f)
     ret = []
     try:
         with open(f) as fp:
@@ -30,7 +31,7 @@ def read_deps(f):
         pass
     return tuple(ret)
 
-def get_deps(f, cache=None, _stack=None):
+def get_deps(f, basedir=None, cache=None, _stack=None):
     if cache is None:
         cache = {}
     if _stack is None:
@@ -43,15 +44,15 @@ def get_deps(f, cache=None, _stack=None):
     if f not in cache:
         ret = []
         _stack += (f,)
-        for rd in read_deps(f):
+        for rd in read_deps(f, basedir):
             ret.append(rd)
-            nd = get_deps(rd, cache, _stack)
+            nd = get_deps(rd, basedir, cache, _stack)
             if nd is None: continue
             ret.extend(nd)
         cache[f] = frozenset(ret)
     return cache[f]
 
-def get_link_deps(f, cache=None, lcache=None, _stack=None):
+def get_link_deps(f, basedir=None, cache=None, lcache=None, _stack=None):
     if cache is None:
         cache = {}
     if lcache is None:
@@ -67,17 +68,18 @@ def get_link_deps(f, cache=None, lcache=None, _stack=None):
         ret = []
         _stack += (f,)
         src = OBJECT_RE.sub(SOURCE_SUB, f)
-        deps = get_deps(src, cache)
+        deps = get_deps(src, basedir, cache)
         for d in deps:
             s = HEADER_RE.sub(SOURCE_SUB, d)
             o = SOURCE_RE.sub(OBJECT_SUB, s)
             ret.append(o)
             ret.extend(HEADER_RE.sub(OBJECT_SUB, i)
-                       for i in get_deps(s, cache))
+                       for i in get_deps(s, basedir, cache))
         lcache[f] = frozenset(ret)
     return lcache[f]
 
 def main():
+    basedir = os.environ.get('INCLUDE')
     cache, lcache, progs, proglist = {}, {}, {}, []
     for f in sys.argv[1:]:
         if PROGRAM_RE.match(f):
@@ -86,14 +88,14 @@ def main():
             progs.setdefault(prog, []).append(dep)
             continue
         if not ALLOWED_RE.match(f): continue
-        l = get_deps(f, cache)
+        l = get_deps(f, basedir, cache)
         if not l: continue
         print (SOURCE_RE.sub(OBJECT_SUB, f) + ': ' + ' '.join(l))
     for p in proglist:
         l = set()
         for f in progs[p]:
             l.add(f)
-            l.update(get_link_deps(f, cache, lcache))
+            l.update(get_link_deps(f, basedir, cache, lcache))
         if not l: continue
         print (p + ': ' + ' '.join(l))
 
