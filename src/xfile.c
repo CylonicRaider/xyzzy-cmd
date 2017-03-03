40e94e28304fa50eea5547b77ee7211efb5fb55f
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-03-03 19:37:00 +0100

    Outline buffer management into own methods

--- src/xfile.c
+++ src/xfile.c
@@ -8,6 +8,18 @@
 
 #define BUFSIZE 4096
 
+static inline int fbuf_init(struct fbuf *buf) {
+    buf->data = malloc(BUFSIZE);
+    if (buf->data == NULL) return -1;
+    buf->cap = BUFSIZE;
+    buf->begin = 0;
+    buf->len = 0;
+    return 0;
+}
+static inline void fbuf_del(struct fbuf *buf) {
+    free(buf->data);
+}
+
 XFILE *xfdopen(int fd, int flags) {
     if (flags & ~_XFILE_MASK) {
         errno = EINVAL;
@@ -17,23 +29,15 @@ XFILE *xfdopen(int fd, int flags) {
     if (file == NULL) return NULL;
     file->fd = fd;
     file->flags = flags;
-    file->rdbuf.data = malloc(BUFSIZE);
-    if (file->rdbuf.data == NULL) {
+    if (fbuf_init(&file->rdbuf) == -1) {
         free(file);
         return NULL;
     }
-    file->rdbuf.cap = BUFSIZE;
-    file->rdbuf.begin = 0;
-    file->rdbuf.len = 0;
-    file->wrbuf.data = malloc(BUFSIZE);
-    if (file->wrbuf.data == NULL) {
-        free(file->rdbuf.data);
+    if (fbuf_init(&file->wrbuf) == -1) {
+        fbuf_del(&file->rdbuf);
         free(file);
         return NULL;
     }
-    file->wrbuf.cap = BUFSIZE;
-    file->wrbuf.begin = 0;
-    file->wrbuf.len = 0;
     return file;
 }
 
@@ -60,8 +64,8 @@ int xfflush(XFILE *f) {
 int xfclose(XFILE *f) {
     int ret = 0;
     if (xfflush(f) == -1) ret = -1;
-    free(f->rdbuf.data);
-    free(f->wrbuf.data);
+    fbuf_del(&f->rdbuf);
+    fbuf_del(&f->wrbuf);
     if (f->flags & XFILE_CLOSEFD) {
         if (close(f->fd) == -1) ret = -1;
     }
